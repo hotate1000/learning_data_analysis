a44263676eb5a48fd15d213cf1ecd1816aa3dd92
hotate1000 <ooo.336.sss@gmail.com>
2023-03-23 14:38:50 +0900

    グラフの作成

---
+++ chap4/char4-1_2.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt;
+import seaborn as sns;
+
+sns.set(font=["Meiryo", "Yu Gothic", "Hiragino Maru Gothic Pro"]);
+plt.plot([0, 100, 200],[100, 0, 200]);
+plt.title("タイトル");
+plt.show();

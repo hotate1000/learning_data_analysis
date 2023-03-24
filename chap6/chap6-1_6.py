3ef67724f8d34db373d48b46f7e19d2f6aab2daa
hotate1000 <ooo.336.sss@gmail.com>
2023-03-24 12:08:18 +0900

    誤字修正

--- chap6/chap6-1_6.py
+++ chap6/chap6-1_6.py
@@ -21,7 +21,7 @@ plt.show();
 df.plot.scatter(x="数学", y="社会", c="b");
 plt.title("数学と社会の関係性");
 plt.show();
-print("-----"):
+print("-----");
 
 print("数学と理科=", df.corr()["数学"]["理科"]);
 print("数学と社会=", df.corr()["数学"]["社会"]);
@@ -41,4 +41,3 @@ plt.show();
 sns.jointplot(data=df, x="数学", y="社会", kind="reg", line_kws={"color":"red"});
 plt.show();
 print("-----");
-
